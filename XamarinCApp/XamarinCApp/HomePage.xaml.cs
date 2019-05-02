c16d90b2ff188c182ce4161079915b4c0db593e0
Manjunath Bagade <themanju.bagade@gmail.com>
2019-05-02 15:29:26 +0530

    xamarin csharp

---
+++ XamarinCApp/XamarinCApp/HomePage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinCApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class HomePage : ContentPage
+	{
+		public HomePage ()
+		{
+            var name = new Entry
+            {
+                Placeholder="name"
+            };
+            var email = new Entry
+            {
+                Placeholder="email"
+            };
+            var password = new Entry
+            {
+                Placeholder = "password"
+            };
+            var Mnumber = new Entry
+            {
+                Placeholder="Mobile no"
+            };
+
+            var Singup = new Button
+            {
+                Text = "summit"
+            };
+
+            Singup.Clicked += (Object sender, EventArgs e) =>
+            {
+                DisplayAlert("suceess", "register", "ok");
+            };
+
+            Content = new StackLayout
+            {
+                Padding =30,
+                Spacing =10,
+                Children =
+                {
+                    new Label
+                    {
+                        Text="Register form" , FontSize=Device.GetNamedSize (NamedSize.Large,typeof(Label)), HorizontalOptions=LayoutOptions.Center
+                    },
+                    name,
+                    email,
+                    password,
+                    Mnumber,
+                    Singup
+                }
+            };
+            
+			//InitializeComponent ();
+		}
+	}
+}
---
+++ XamarinCApp/XamarinCApp/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinCApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            var emai = new Entry
+            {
+               Placeholder ="user name",
+               PlaceholderColor =Color.Black
+               
+               
+            };
+            var password = new Entry
+            {
+                Placeholder = "pass word",
+                PlaceholderColor=Color.Black
+                
+            };
+            var signup = new Button
+            {
+                Text="sign up",
+                TextColor=Color.Red,
+                FontAttributes=FontAttributes.Bold
+                
+            };
+            signup.Clicked += (object sender, EventArgs e) =>
+            {
+                DisplayAlert("good news", "good morning", "ok");
+                Navigation.PushModalAsync(new HomePage());
+            };
+            // InitializeComponent();
+            Content = new StackLayout
+            {
+                Padding = 30,
+                Spacing = 10,
+                Children = {
+          new Label { Text = "Fundoo App", FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label)), HorizontalOptions = LayoutOptions.Center },
+          emai,
+          password,
+          signup
+        }
+            };
+        }
+    }
+}
